44639bcc7201a7477668a635ce903cad7335a607
Michael Cote <michaelcote7@outlook.com>
2021-06-08 21:35:22 -0700

    neumorphic ui

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  
+  return (
+    <div className="container-fluid neu-app">
+      <div className="row">
+        <div className="col">
+          <p>hello cote</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+  
+  return (
+    <div>
+      nav works!
+    </div>
+  );
+}
+
+export default Nav;
