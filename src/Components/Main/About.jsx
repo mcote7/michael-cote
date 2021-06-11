2ebac81280ebb0029fd520cd7036f20ab06897ff
Michael Cote <michaelcote7@outlook.com>
2021-06-11 16:15:56 -0700

    .

--- src/Components/Main/About.jsx
+++ src/Components/Main/About.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 const About = () => {
   return (
     // parent is '.row' //
-    <div className="self-wrap col-md-6">
-      <div className="self m-auto">
-        <span className="back-1"></span>
-        <span className="back-2"></span>
+    <div className="col-xl-6 col-lg-8 col-md-10">
+      <div className="self-wrap">
+        <div className="self-text">
+          <h3><i class="fa fa-globe" aria-hidden="true"></i> hello world</h3>
+          <p>i'm michael, a software developer in Seattle.</p>
+        </div>
+        <div className="self">
+          <span className="back-1"></span>
+          <span className="back-2"></span>
+        </div>
       </div>
     </div>
   );
