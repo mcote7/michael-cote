09a1e0dac9648d8fef40d8f28adb979b9e8e5e8c
Michael Cote <michaelcote7@outlook.com>
2021-06-11 20:15:38 -0700

    dark mode done

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ const App = () => {
 
   const [darkMode, setDarkMode] = useState(false);
 
+  // 🚧 TODO set color theme (3) //
+
   useEffect(()=> {
     console.log("dark mode?", darkMode)
-    // 
+    // 🌗 set dark mode 
+    const root = document.documentElement;
     
+    root?.style.setProperty("--bgcolor", darkMode ? "#444444" : "#e4ebf5");
+    root?.style.setProperty("--dark-shadow", darkMode ? "#363636" : "#bec8e4");
+    root?.style.setProperty("--light-shadow", darkMode ? "#525252" : "#ffffff");
+    
+    root?.style.setProperty("--text-color-primary", darkMode ? "rgba(255, 255, 255, 0.93)" : "rgba(0, 0, 0, 0.93)");
+    root?.style.setProperty("--text-color-secondary", darkMode ? "rgba(255, 255, 255, 0.7)" : "rgba(0, 0, 0, 0.7)");
+    root?.style.setProperty("--text-color-disabled", darkMode ? "rgba(255, 255, 255, 0.45)" : "rgba(0, 0, 0, 0.45)");
+    
+    root?.style.setProperty("--bg-color-disabled", darkMode ? "#727272" : "#dee5e8");
   },[darkMode]);
   
   return (
