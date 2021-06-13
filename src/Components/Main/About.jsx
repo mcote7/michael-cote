73eea2b2204393cbedf945bf9c71745a6b2d0a92
Michael Cote <michaelcote7@outlook.com>
2021-06-12 18:31:51 -0700

    .

--- src/Components/Main/About.jsx
+++ src/Components/Main/About.jsx
@@ -8,22 +8,23 @@ const About = ({darkMode, primaryColor, isBlueActive, isGreenActive, isOrangeAct
   return (<React.Fragment>
     <div className="col-sm-12">
       
-      <div className="self-wrap my-5">
+      <div className="self-wrap row mb-5">
         
-        <div className="self-text">
-          <h3><i className="fa fa-globe" aria-hidden="true"></i> hello world</h3>
-          <p>i'm michael, a software developer in Seattle.</p>
-        </div>
-        <div className="self">
+        <div className="self-img col-lg-6">
           <span className="back-1"></span>
           <span className="back-2"></span>
+        </div>
+        <div className="self-text col-lg-6 mt-5">
+          <h3><i className="fa fa-globe" aria-hidden="true"></i> hello world</h3>
+          <p>i'm michael, a software developer in Seattle.</p>
         </div>
         
       </div>
       
-      <div className="stats-wrap my-5">
+      {/* good */}
+      <div className="stats-wrap row my-5">
         
-        <div className="git-card">
+        <div className="git-card col-lg-6">
           {darkMode ? 
             <><img src={`https://github-readme-stats.vercel.app/api?username=mcote7&show_icons=true&theme=dark&icon_color=${primaryColor.substring(1)}&border_color=444444&bg_color=444444`} alt="stats"/>
             <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mcote7&hide_title=true&layout=compact&theme=dark&border_color=444444&bg_color=444444" alt="langs"/></>:
@@ -31,9 +32,8 @@ const About = ({darkMode, primaryColor, isBlueActive, isGreenActive, isOrangeAct
             <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mcote7&hide_title=true&layout=compact&theme=greywhite&border_color=e4ebf5&bg_color=e4ebf5" alt="langs"/></>
             }
         </div>
-          
-        <div className="role-card">
-          
+        
+        <div className="role-card col-lg-6">
           <div className="role-title">
             {isBlueActive && <div className="angular blue"></div>}
             {isGreenActive && <div className="angular green"></div>}
@@ -43,7 +43,6 @@ const About = ({darkMode, primaryColor, isBlueActive, isGreenActive, isOrangeAct
               <p className="sub">software developer @</p>
             </div>
           </div>
-          
           <div className="role-body">
             <a 
               target="_blank" 
