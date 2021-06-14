4dc0bfa22cf3af1e3ce78495285d09aea3bdd179
Michael Cote <michaelcote7@outlook.com>
2021-06-13 17:56:31 -0700

    .

---
+++ src/Config/skills.js
@@ -0,0 +1,32 @@
+
+export const LANGUAGES_LIST = [
+  'JavaScript',
+  'TypeScript',
+  'HTML5',
+  'CSS3',
+  'C#',
+  'Python'
+];
+
+export const FRONT_END_TECH = [
+  'Angular',
+  'React',
+  'SASS',
+  'Bootstrap',
+  'Node'
+];
+
+export const DATABASE_LIST = [
+  'Firebase',
+  'MySQL',
+  'MongoDB'
+];
+
+export const TOOLS_LIST = [
+  'VSCode',
+  'GitHub',
+  'Azure devOps',
+  'SQL workbench',
+  'PostMan',
+  'Adobe'
+];
