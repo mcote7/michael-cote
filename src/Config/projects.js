b1b647f8d3bf8a361a18a49086e191a7b32d924a
Michael Cote <michaelcote7@outlook.com>
2021-06-15 22:59:32 -0700

    .

--- src/Config/projects.js
+++ src/Config/projects.js
@@ -4,6 +4,7 @@ export const PROJ = [
     title: 'O-SHOP',
     date: '5/21',
     url: 'https://o-shop-c0e98.web.app/',
+    repoUrl: 'https://github.com/mcote7/o-shop',
     info: 'A mock produce storefront, e-commerce functionality.',
     tech: ['Angular', 'RXJS', 'SASS', 'Firebase']
   },
