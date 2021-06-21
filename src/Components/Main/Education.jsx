23a70c34664d6de490092b2cfda63ab2e27ab048
Michael Cote <michaelcote7@outlook.com>
2021-06-21 10:36:49 -0700

    .

--- src/Components/Main/Education.jsx
+++ src/Components/Main/Education.jsx
@@ -15,7 +15,10 @@ const Education = () => {
               id={`cert${idx}`} 
               className={idx % 2 === 0 ? 'container left' : 'container right'}>
               <div className="content">
-                {idx % 2 === 0 ? <div className="cert-title cert-left"><sup>{cert.date}</sup>&nbsp;{cert.title}</div> : <div className="cert-title">{cert.title}&nbsp;<sup>{cert.date}</sup></div>}
+                {idx % 2 === 0 ? 
+                <div className="cert-title cert-left"><sup>{cert.date}</sup>&nbsp;{cert.title}</div> :
+                <div className="cert-title">{cert.title}&nbsp;<sup>{cert.date}</sup></div>}
+                
                 <div className={`cert-img ${cert.image}`}></div>
               </div>
             </div>
