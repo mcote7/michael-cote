c2116a37cc8a92f070219cf4217f32b5f0407e2f
Michael Cote <michaelcote7@outlook.com>
2021-08-01 13:11:08 -0700

    wakatime update

--- src/Components/Main/Education.jsx
+++ src/Components/Main/Education.jsx
@@ -31,14 +31,23 @@ const Education = ({darkMode}) => {
         })}
       </div>
       
-      <div id="metrics" style={{textAlign: 'center'}}>development 📈 metrics</div>
+      <div id="metrics" style={{textAlign: 'center'}}><small style={{fontWeight: 'bold'}}>development 📈 metrics</small></div>
       { darkMode ? 
       <div className="waka-wrap">
       <figure><embed src="https://wakatime.com/share/@b94737ec-5326-4fb7-9eb6-e13175e91bf8/2791a21b-5877-4d74-a468-cd5c8471e133.svg"></embed></figure>
       </div>
       :
-      <div className="row waka-wrap">
+      <div className="waka-wrap">
         <figure><embed src="https://wakatime.com/share/@b94737ec-5326-4fb7-9eb6-e13175e91bf8/fe39fa84-48a9-4876-99e2-7c5786ac952d.svg"></embed></figure>
+      </div> } 
+      
+      { darkMode ? 
+      <div className="waka-wrap">
+        <figure><embed src="https://wakatime.com/share/@b94737ec-5326-4fb7-9eb6-e13175e91bf8/fd02c9ed-da9d-4166-b391-2a4ac82d83d2.svg"></embed></figure>
+      </div>
+      :
+      <div className="waka-wrap">
+        <figure><embed src="https://wakatime.com/share/@b94737ec-5326-4fb7-9eb6-e13175e91bf8/fc7e751b-9f82-4c11-b1fa-87e66a9157a4.svg"></embed></figure>
       </div> } 
     </div>
   );
