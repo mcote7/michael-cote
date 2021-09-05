eca1a5352be5801666e03cdaaec6f8ac79a643cf
Michael Cote <michaelcote7@outlook.com>
2021-09-05 14:29:56 -0700

    .

--- src/Config/projects.js
+++ src/Config/projects.js
@@ -1,12 +1,20 @@
 
 export const PROJ = [
   {
-    title: 'O-SHOP',
-    date: '5/21',
-    url: 'https://o-shop-c0e98.web.app/',
-    repoUrl: 'https://github.com/mcote7/o-shop',
-    info: 'Mock produce storefront, NgRx Redux store.',
-    tech: ['Angular', 'NgRx', 'Firebase']
+    title: 'FACEBOOP',
+    date: 'current',
+    url: 'https://mcote7.github.io/faceboop/',
+    repoUrl: 'https://github.com/mcote7/faceboop',
+    info: 'Social media experiment.',
+    tech: ['React', 'SaSS', 'Bootstrap']
+  },
+  {
+    title: 'NEWS APP',
+    date: 'current',
+    url: 'https://fake-news-media.web.app/',
+    repoUrl: 'https://github.com/mcote7/fake-news-media',
+    info: 'News media experiment.',
+    tech: ['Angular', 'Firebase', 'SaSS']
   },
   {
     title: 'MENU-CUBE',
@@ -25,12 +33,12 @@ export const PROJ = [
     tech: ['React', 'SaSS', 'Jitsi']
   },
   {
-    title: 'FACEBOOP',
-    date: 'current',
-    url: 'https://mcote7.github.io/faceboop/',
-    repoUrl: 'https://github.com/mcote7/faceboop',
-    info: 'Social media experiment.',
-    tech: ['React', 'SaSS', 'Bootstrap']
+    title: 'O-SHOP',
+    date: '5/21',
+    url: 'https://o-shop-c0e98.web.app/',
+    repoUrl: 'https://github.com/mcote7/o-shop',
+    info: 'Mock produce storefront, NgRx Redux store.',
+    tech: ['Angular', 'NgRx', 'Firebase']
   },
 ];
 
