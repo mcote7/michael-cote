83563254ce3ecf93401fd916304993b264418f05
Michael Cote <michaelcote7@outlook.com>
2022-12-06 18:21:43 -0800

    updated colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,152 +1,157 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import useRS from "radioactive-state";
-import Main from './Components/Main/Main';
-import Nav from './Components/Navigation/Nav';
-import { animateBulb } from './Util/bulb.anime';
-
+import Main from "./Components/Main/Main";
+import Nav from "./Components/Navigation/Nav";
+import { animateBulb } from "./Util/bulb.anime";
 
 const App = () => {
+    const [darkMode, setDarkMode] = useState(true);
 
-  const [darkMode, setDarkMode] = useState(true);
+    const handleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
 
-  const handleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+    useEffect(() => {
+        animateBulb();
 
-  useEffect(()=> {
-    animateBulb();
-    
-    document.body.classList.add('no-transition');
-    
-    const root = document.documentElement;
-    
-    root?.style.setProperty("--bgcolor", darkMode ? "#444444" : "#e4ebf5");
-    root?.style.setProperty("--dark-shadow", darkMode ? "#363636" : "#bec8e4");
-    root?.style.setProperty("--light-shadow", darkMode ? "#525252" : "#ffffff");
-    
-    root?.style.setProperty("--text-color-primary", darkMode ? "rgba(255, 255, 255, 0.93)" : "rgba(0, 0, 0, 0.93)");
-    root?.style.setProperty("--text-color-secondary", darkMode ? "rgba(255, 255, 255, 0.7)" : "rgba(0, 0, 0, 0.7)");
-    root?.style.setProperty("--text-color-disabled", darkMode ? "rgba(255, 255, 255, 0.45)" : "rgba(0, 0, 0, 0.45)");
-    
-    
-    // console.log("dark mode?", darkMode)
-    
-    setTimeout(() => {
-      document.body.classList.remove('no-transition');
-    }, 100);
-    
-  },[darkMode]);
+        document.body.classList.add("no-transition");
 
+        const root = document.documentElement;
 
-  const [colorMode, setColorMode] = useState('blue');
-  const [primaryColor, setPrimaryColor] = useState('#2977ff');
-  
-  const [isBlueActive, setIsBlueActive] = useState('active');
-  const [isGreenActive, setIsGreenActive] = useState('');
-  const [isOrangeActive, setIsOrangeActive] = useState('');
+        root?.style.setProperty("--bgcolor", darkMode ? "#444444" : "#e4ebf5");
+        root?.style.setProperty("--dark-shadow", darkMode ? "#363636" : "#bec8e4");
+        root?.style.setProperty("--light-shadow", darkMode ? "#525252" : "#ffffff");
 
-  useEffect(()=> {
-    
-    document.body.classList.add('no-transition');
-    
-    const root = document.documentElement;
-    
-    if(colorMode === 'blue') {
-      animateBulb();
-      root?.style.setProperty("--primary", "#2978FF");
-      root?.style.setProperty("--primary-dark", "#0151D9");
-      root?.style.setProperty("--primary-light", "#7DADFE");
-      setPrimaryColor('#2978FF');
-      setIsBlueActive('active');
-      setIsGreenActive('');
-      setIsOrangeActive('');
-    }
-    if(colorMode === 'green') {
-      animateBulb();
-      root?.style.setProperty("--primary", "#28C200");
-      root?.style.setProperty("--primary-dark", "#219C00");
-      root?.style.setProperty("--primary-light", "#6BDD4D");
-      setPrimaryColor('#28C200');
-      setIsGreenActive('active');
-      setIsBlueActive('');
-      setIsOrangeActive('');
-    }
-    if(colorMode === 'orange') {
-      animateBulb();
-      root?.style.setProperty("--primary", "#F58300");
-      root?.style.setProperty("--primary-dark", "#C16700");
-      root?.style.setProperty("--primary-light", "#FFB560");
-      setPrimaryColor('#F58300');
-      setIsOrangeActive('active');
-      setIsBlueActive('');
-      setIsGreenActive('');
-    }
-    
-    // console.log("color mode?", colorMode)
-    
-    setTimeout(() => {
-      document.body.classList.remove('no-transition');
-    }, 100);
-    
-  },[colorMode]);
+        root?.style.setProperty(
+            "--text-color-primary",
+            darkMode ? "rgba(255, 255, 255, 0.93)" : "rgba(0, 0, 0, 0.93)"
+        );
+        root?.style.setProperty(
+            "--text-color-secondary",
+            darkMode ? "rgba(255, 255, 255, 0.7)" : "rgba(0, 0, 0, 0.7)"
+        );
+        root?.style.setProperty(
+            "--text-color-disabled",
+            darkMode ? "rgba(255, 255, 255, 0.45)" : "rgba(0, 0, 0, 0.45)"
+        );
 
+        // console.log("dark mode?", darkMode)
 
-  const state = useRS({
-    x: 0,
-    y: 0
-  });
+        setTimeout(() => {
+            document.body.classList.remove("no-transition");
+        });
+    }, [darkMode]);
 
-  const handleMouseMove = (e) => {
-    e.preventDefault();
-    // console.log("coord:", e.clientX, e.clientY)
-    const el = document.getElementById('follow');
-    el.classList.add('on');
-    state.x = e.clientX - 18; // (18) 1/2 width/height of el to center //
-    state.y = e.clientY - 18;
-  };
+    const [colorMode, setColorMode] = useState("blue");
+    const [primaryColor, setPrimaryColor] = useState("#2978FF");
 
-  const handleMouseLeave = (e) => {
-    const el = document.getElementById('follow');
-    el.classList.remove('on');
-  };
+    const [isBlueActive, setIsBlueActive] = useState("active");
+    const [isGreenActive, setIsGreenActive] = useState("");
+    const [isOrangeActive, setIsOrangeActive] = useState("");
 
-  useEffect(()=> {
-    window.addEventListener('blur', left);
-    window.addEventListener('focus', back);
-  },[]);
+    useEffect(() => {
+        document.body.classList.add("no-transition");
 
-  const left = (e) => {
-    document.title = 'see you soon 🚀';
-  };
+        const root = document.documentElement;
 
-  const back = (e) => {
-    document.title = 'welcome back ✨';
-  };
+        if (colorMode === "blue") {
+            animateBulb();
+            root?.style.setProperty("--primary", "#2978FF");
+            root?.style.setProperty("--primary-dark", "#0151D9");
+            root?.style.setProperty("--primary-light", "#7DADFE");
+            setPrimaryColor("#2978FF");
+            setIsBlueActive("active");
+            setIsGreenActive("");
+            setIsOrangeActive("");
+        }
+        if (colorMode === "green") {
+            animateBulb();
+            root?.style.setProperty("--primary", "#28C270");
+            root?.style.setProperty("--primary-dark", "#219C59");
+            root?.style.setProperty("--primary-light", "#6BDD90");
+            setPrimaryColor("#28C270");
+            setIsGreenActive("active");
+            setIsBlueActive("");
+            setIsOrangeActive("");
+        }
+        if (colorMode === "orange") {
+            animateBulb();
+            root?.style.setProperty("--primary", "#FF163E");
+            root?.style.setProperty("--primary-dark", "#C1163E");
+            root?.style.setProperty("--primary-light", "#FF3E70");
+            setPrimaryColor("#FF163E");
+            setIsOrangeActive("active");
+            setIsBlueActive("");
+            setIsGreenActive("");
+        }
 
-  
-  return (
-    <div className="container-fluid neu-app">
-      
-      <Nav 
-        darkMode={darkMode} 
-        handleDarkMode={handleDarkMode} 
-        setColorMode={setColorMode} 
-        isBlueActive={isBlueActive} 
-        isGreenActive={isGreenActive} 
-        isOrangeActive={isOrangeActive} />
-        
-      <Main 
-        darkMode={darkMode} 
-        primaryColor={primaryColor} 
-        isBlueActive={isBlueActive} 
-        isGreenActive={isGreenActive} 
-        isOrangeActive={isOrangeActive} 
-        handleMouseMove={handleMouseMove} 
-        handleMouseLeave={handleMouseLeave} />
-                                              
-      <div id="follow" className="follow" style={{top: `${state.y}px`, left: `${state.x}px`}}></div>
-    </div>
-  );
+        // console.log("color mode?", colorMode)
+
+        setTimeout(() => {
+            document.body.classList.remove("no-transition");
+        });
+    }, [colorMode]);
+
+    const state = useRS({
+        x: 0,
+        y: 0,
+    });
+
+    const handleMouseMove = (e) => {
+        e.preventDefault();
+        // console.log("coord:", e.clientX, e.clientY)
+        const el = document.getElementById("follow");
+        el.classList.add("on");
+        state.x = e.clientX - 18; // (18) 1/2 width/height of el to center //
+        state.y = e.clientY - 18;
+    };
+
+    const handleMouseLeave = (e) => {
+        const el = document.getElementById("follow");
+        el.classList.remove("on");
+    };
+
+    useEffect(() => {
+        window.addEventListener("blur", left);
+        window.addEventListener("focus", back);
+    }, []);
+
+    const left = (e) => {
+        document.title = "see you soon 🚀";
+    };
+
+    const back = (e) => {
+        document.title = "welcome back ✨";
+    };
+
+    return (
+        <div className="container-fluid neu-app">
+            <Nav
+                darkMode={darkMode}
+                handleDarkMode={handleDarkMode}
+                setColorMode={setColorMode}
+                isBlueActive={isBlueActive}
+                isGreenActive={isGreenActive}
+                isOrangeActive={isOrangeActive}
+            />
+
+            <Main
+                darkMode={darkMode}
+                primaryColor={primaryColor}
+                isBlueActive={isBlueActive}
+                isGreenActive={isGreenActive}
+                isOrangeActive={isOrangeActive}
+                handleMouseMove={handleMouseMove}
+                handleMouseLeave={handleMouseLeave}
+            />
+
+            <div
+                id="follow"
+                className="follow"
+                style={{ top: `${state.y}px`, left: `${state.x}px` }}
+            ></div>
+        </div>
+    );
 };
 
 export default App;
--- src/Components/Main/About.jsx
+++ src/Components/Main/About.jsx
@@ -1,96 +1,153 @@
-import React from 'react';
-import { SKILLS } from '../../Config/skills';
+import React from "react";
+import { SKILLS } from "../../Config/skills";
 
+const About = ({
+    darkMode,
+    primaryColor,
+    isBlueActive,
+    isGreenActive,
+    isOrangeActive,
+    handleMouseMove,
+    handleMouseLeave,
+}) => {
+    return (
+        <React.Fragment>
+            <div className="col col-sm-12">
+                {/* intro */}
+                <div className="self-wrap row">
+                    <div className="self-img col-lg-6">
+                        <span className="back-1"></span>
+                        <span className="back-2"></span>
+                    </div>
+                    <div className="self-text col-lg-6 mt-5">
+                        <h3>
+                            <i className="fa fa-globe" aria-hidden="true"></i> hello world
+                        </h3>
+                        <p>i'm michael, a software developer in Seattle.</p>
+                    </div>
+                </div>
 
-const About = ({darkMode, primaryColor, isBlueActive, isGreenActive, isOrangeActive, handleMouseMove, handleMouseLeave}) => {
+                {/* stats */}
+                <div className="stats-wrap row">
+                    <div className="col-lg-6">
+                        {darkMode ? (
+                            <React.Fragment>
+                                <div className="git-card">
+                                    <img
+                                        src={`https://github-readme-stats.vercel.app/api?username=mcote7&show_icons=true&theme=dark&icon_color=${primaryColor.substring(
+                                            1
+                                        )}&border_color=444444&bg_color=444444&text_color=E1E1E1&title_color=${primaryColor.substring(
+                                            1
+                                        )}`}
+                                        alt="stats"
+                                    />
+                                </div>
+                                <div className="git-card">
+                                    <div className="most-used dark">📊 Most used languages</div>
+                                    <img
+                                        src="https://github-readme-stats.vercel.app/api/top-langs/?username=mcote7&hide_title=true&layout=compact&theme=dark&border_color=444444&bg_color=444444&text_color=E1E1E1"
+                                        alt="langs"
+                                    />
+                                </div>
+                            </React.Fragment>
+                        ) : (
+                            <React.Fragment>
+                                <div className="git-card">
+                                    <img
+                                        src={`https://github-readme-stats.vercel.app/api?username=mcote7&show_icons=true&icon_color=${primaryColor.substring(
+                                            1
+                                        )}&theme=graywhite&border_color=e4ebf5&bg_color=e4ebf5&title_color=${primaryColor.substring(
+                                            1
+                                        )}`}
+                                        alt="stats"
+                                    />
+                                </div>
+                                <div className="git-card">
+                                    <div className="most-used">📊 Most used languages</div>
+                                    <img
+                                        src="https://github-readme-stats.vercel.app/api/top-langs/?username=mcote7&hide_title=true&layout=compact&theme=greywhite&border_color=e4ebf5&bg_color=e4ebf5"
+                                        alt="langs"
+                                    />
+                                </div>
+                            </React.Fragment>
+                        )}
+                    </div>
 
-  return (<React.Fragment>
-    <div className="col col-sm-12">
-      
-      {/* intro */}
-      <div className="self-wrap row">
-        <div className="self-img col-lg-6">
-          <span className="back-1"></span>
-          <span className="back-2"></span>
-        </div>
-        <div className="self-text col-lg-6 mt-5">
-          <h3><i className="fa fa-globe" aria-hidden="true"></i> hello world</h3>
-          <p>i'm michael, a software developer in Seattle.</p>
-        </div>
-      </div>
-      
-      {/* stats */}
-      <div className="stats-wrap row">
-        <div className="col-lg-6">
-          {darkMode ? 
-            <React.Fragment>
-              <div className="git-card">
-                <img src={`https://github-readme-stats.vercel.app/api?username=mcote7&show_icons=true&theme=dark&icon_color=${primaryColor.substring(1)}&border_color=444444&bg_color=444444&text_color=E1E1E1&title_color=${primaryColor.substring(1)}`} alt="stats"/>
-              </div>
-              <div className="git-card">
-                <div className="most-used dark">📊 Most used languages</div>
-                <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mcote7&hide_title=true&layout=compact&theme=dark&border_color=444444&bg_color=444444&text_color=E1E1E1" alt="langs"/>
-              </div>
-            </React.Fragment>
-            : 
-            <React.Fragment>
-              <div className="git-card">
-                <img src={`https://github-readme-stats.vercel.app/api?username=mcote7&show_icons=true&icon_color=${primaryColor.substring(1)}&theme=graywhite&border_color=e4ebf5&bg_color=e4ebf5`} alt="stats"/>
-              </div>
-              <div className="git-card">
-                <div className="most-used">📊 Most used languages</div>
-                <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mcote7&hide_title=true&layout=compact&theme=greywhite&border_color=e4ebf5&bg_color=e4ebf5" alt="langs"/>
-              </div>
-            </React.Fragment>}
-        </div>
-        
-        <div className="role-card col-lg-6">
-          <div className="role-title">
-            {isBlueActive && <div className="angular blue"></div>}
-            {isGreenActive && <div className="angular green"></div>}
-            {isOrangeActive && <div className="angular"></div>}
-            <div>
-              <p>current role: </p>
-              <p className="sub">software developer @</p>
+                    <div className="role-card col-lg-6">
+                        <div className="role-title">
+                            {isBlueActive && <div className="angular blue"></div>}
+                            {isGreenActive && <div className="angular green"></div>}
+                            {isOrangeActive && <div className="angular"></div>}
+                            <div>
+                                <p>current role: </p>
+                                <p className="sub">software developer @</p>
+                            </div>
+                        </div>
+                        <div className="role-body">
+                            <a
+                                target="_blank"
+                                title="https://videonet.us/"
+                                rel="noopener noreferrer"
+                                href="https://videonet.us/"
+                            >
+                                videonet holdings&nbsp;
+                                <i className="fa fa-external-link-square" aria-hidden="true"></i>
+                            </a>
+                            <div className="quote">
+                                <small>
+                                    We are building secure business solutions using modern
+                                    communication tools like video conferencing, live streaming and
+                                    real-time screen sharing.
+                                </small>
+                            </div>
+                            <div className="quote via3">
+                                <small>check out our platform:</small>
+                                <a
+                                    target="_blank"
+                                    title="https://unity.via3.com/"
+                                    rel="noopener noreferrer"
+                                    href="https://unity.via3.com/"
+                                >
+                                    via3&nbsp;
+                                    <i
+                                        className="fa fa-external-link-square"
+                                        aria-hidden="true"
+                                    ></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* skills */}
+                <div
+                    className="skills-wrap row"
+                    onMouseLeave={(e) => handleMouseLeave(e)}
+                    onMouseMove={(e) => handleMouseMove(e)}
+                >
+                    {SKILLS &&
+                        SKILLS.map((sk, idx) => {
+                            if (
+                                sk === "TypeScript" ||
+                                sk === "Angular" ||
+                                sk === "SaSS" ||
+                                sk === "RxJS"
+                            )
+                                return (
+                                    <div key={idx} className="skill-badge key">
+                                        {sk}
+                                    </div>
+                                );
+                            return (
+                                <div key={idx} className="skill-badge">
+                                    {sk}
+                                </div>
+                            );
+                        })}
+                </div>
             </div>
-          </div>
-          <div className="role-body">
-            <a 
-              target="_blank" 
-              title="https://videonet.us/"
-              rel="noopener noreferrer" 
-              href="https://videonet.us/">videonet holdings&nbsp;
-              <i className="fa fa-external-link-square" aria-hidden="true"></i>
-            </a>
-            <div className="quote">
-              <small>We are building secure business solutions using modern communication tools like video conferencing, live streaming and real-time screen sharing.</small>
-            </div>
-            <div className="quote via3">
-              <small>check out our platform:</small>
-              <a 
-                target="_blank" 
-                title="https://unity.via3.com/"
-                rel="noopener noreferrer" 
-                href="https://unity.via3.com/">via3&nbsp;
-                <i className="fa fa-external-link-square" aria-hidden="true"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      {/* skills */}
-      <div 
-        className="skills-wrap row"
-        onMouseLeave={(e)=>handleMouseLeave(e)}
-        onMouseMove={(e)=>handleMouseMove(e)}>
-        {SKILLS && SKILLS.map((sk, idx) => {
-          if(sk === 'TypeScript' || sk === 'Angular' || sk === 'SaSS' || sk === 'RxJS') return <div key={idx} className="skill-badge key">{sk}</div>
-          return <div key={idx} className="skill-badge">{sk}</div>
-        })}
-      </div>
-    </div>
-  </React.Fragment>);
+        </React.Fragment>
+    );
 };
 
 export default About;
